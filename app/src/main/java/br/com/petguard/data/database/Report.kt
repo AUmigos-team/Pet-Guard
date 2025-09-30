b3aadcaf82d0606ca5eef87e8b544783d2d8e5c2
Eduardo Soares <eduardo_soares001@outlook.com>
2025-09-30 16:08:33 -0300

    Adding the base tables

---
+++ app/src/main/java/br/com/petguard/data/database/Report.kt
@@ -0,0 +1,38 @@
+package br.com.petguard.data.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "report")
+data class Report (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @ColumnInfo(name = "address")
+    val address: String? = null,
+
+    @ColumnInfo(name = "description")
+    val description: String? = null,
+
+    @ColumnInfo(name = "latitude")
+    val latitude: Double? = null,
+
+    @ColumnInfo(name = "longitude")
+    val longitude: Double? = null,
+
+    @ColumnInfo(name = "photoPath")
+    val photoPath: String? = null,
+
+    @ColumnInfo(name = "videoPath")
+    val videoPath: String? = null,
+
+    @ColumnInfo(name = "status")
+    val status: String = "PENDENTE",
+
+    @ColumnInfo(name = "createdAt")
+    val createdAt: String? = null,
+
+    @ColumnInfo(name = "updateAt")
+    val updateAt: String? = null
+)
