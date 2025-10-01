09409e84faaa59e5da3a402d2768aea71ae4bd33
Eduardo Soares <eduardo_soares001@outlook.com>
2025-10-01 16:11:39 -0300

    ending splash screen

--- app/src/main/java/br/com/petguard/ui/screens/SplashScreen.kt
+++ app/src/main/java/br/com/petguard/ui/screens/SplashScreen.kt
@@ -1,33 +1,63 @@
 package br.com.petguard.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import br.com.petguard.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    val ralewayDots = FontFamily(Font(R.font.raleway_dots_regular))
+    val windSong = FontFamily(Font(R.font.windsong_regular))
+
     Box(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(Color(0xFF7E8C54)),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "GUARD Pet",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xFF7E8C54)
-        )
+            Text(
+                text = "GUARD",
+                fontSize = 55.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = ralewayDots,
+                color = Color.White
+            )
+
+            Text(
+                text = "Pet",
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = windSong,
+                color = Color.White,
+                modifier = Modifier
+                    .offset(x = (70).dp, y = 25.dp)
+            )
+
     }
 
-    androidx.compose.runtime.LaunchedEffect(Unit) {
-        kotlinx.coroutines.delay(2000)
+    LaunchedEffect(Unit) {
+        delay(15000)
         navController.navigate("login")
     }
 }
