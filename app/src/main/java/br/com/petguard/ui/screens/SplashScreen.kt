09079cd942826dba95c3d77bf4965fb2d1e402e4
Eduardo Soares <eduardo_soares001@outlook.com>
2025-10-12 14:55:46 -0300

    Resolução bug de login

--- app/src/main/java/br/com/petguard/ui/screens/SplashScreen.kt
+++ app/src/main/java/br/com/petguard/ui/screens/SplashScreen.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.traceEventEnd
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -17,12 +19,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.com.petguard.R
+import br.com.petguard.data.database.AppDatabase
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
     val ralewayDots = FontFamily(Font(R.font.raleway_dots_regular))
     val windSong = FontFamily(Font(R.font.windsong_regular))
+    val context = LocalContext.current
 
     Box(
         modifier = Modifier
@@ -52,6 +57,17 @@ fun SplashScreen(navController: NavController) {
 
     LaunchedEffect(Unit) {
         delay(2000)
-        navController.navigate("login")
+
+        val database = AppDatabase.getDatabase(context)
+        val user = database.userDao().getLoggedUser()
+        if(FirebaseAuth.getInstance().currentUser != null && user != null) {
+            navController.navigate("home") {
+                popUpTo("splash") {inclusive = true}
+            }
+        }else{
+            navController.navigate("login") {
+                popUpTo("splash") {inclusive = true}
+            }
+        }
     }
 }
